5d5499bf25c1e56fa7ae2078b313a34cb89e7e17
Vinicius Montes <viniciusmontes@outlook.com.br>
2023-11-16 14:40:19 -0300

    GameRepository

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameMinDTO {
+
+	private Long id;
+	private String title;
+	private Integer year;
+	private String imgUrl;
+	private String shotDescription;
+
+	public GameMinDTO() {
+	}
+
+	public GameMinDTO(Game entity) {
+		id = entity.getId();
+		title = entity.getTitle();
+		year = entity.getYear();
+		imgUrl = entity.getImgUrl();
+		shotDescription = entity.getShortDescription();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getShotDescription() {
+		return shotDescription;
+	}
+
+}
