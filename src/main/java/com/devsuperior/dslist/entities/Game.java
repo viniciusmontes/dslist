a8de373d1f1ab5b051323de949216203cf59b201
Vinicius Montes <viniciusmontes@outlook.com.br>
2023-11-16 13:12:47 -0300

    Project created

---
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -0,0 +1,16 @@
+package com.devsuperior.dslist.entities;
+
+public class Game {
+	
+	
+	private Long id;
+	private String title;
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	private String shortDescription;
+	private String longDescription;
+
+}
